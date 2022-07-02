a90729d909c73b3c6642b4fe73e63c988fd575b8
Mamdouh <mamdouhzaq@gmail.com>
2022-07-02 11:47:25 +0300

    add template class

---
+++ classes/Template.php
@@ -0,0 +1,29 @@
+<?php
+
+class Template
+{
+    private string $template;
+
+    private array $variables = [];
+
+    /**
+     * @param string $string
+     */
+    public function __construct(string $template)
+    {
+        $this->template = $template;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->variables[$name] = $value;
+    }
+
+    public function __toString()
+    {
+        extract($this->variables);
+        ob_start();
+        include $this->template;
+        return ob_get_clean();
+    }
+}
--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-
-use Check24\Classes\Database;
-
-require_once '../vendor/autoload.php';
-
-$db = Database::getInstance();
-
-try {
-    $conn = $db->getConnection();
-    echo 'here';
-} catch (Exception $e) {
-    die($e->getMessage());
-}
-
+require_once './core/init.php';

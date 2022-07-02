17f2f6fe465fd3f2c383defd8c60ae58155d0c85
Mamdouh <mamdouhzaq@gmail.com>
2022-07-02 10:59:46 +0300

    add database class

---
+++ classes/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Classes;
+use \PDO;
+
+class Database
+{
+    public static $instance = null;
+
+    private PDO $pdo;
+
+    private string $username = 'root';
+    private string $password = 'P@ssw0rd';
+    private string $dbname = 'check24';
+    private string $host = 'localhost';
+    private string $port = '3306';
+
+    private function __construct()
+    {
+        $this->pdo = new PDO(
+            'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname,
+            $this->username,
+            $this->password
+        );
+    }
+
+    public static function getInstance(): Database
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->pdo;
+    }
+
+}
